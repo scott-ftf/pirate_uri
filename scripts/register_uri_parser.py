ce4c361187cfff119f37edc5b8ed75eb52feec7f
orion <scott_fromthefuture@pm.me>
2024-05-19 22:36:04 -0600

    UTF-8 encode memo

--- scripts/register_uri_parser.py
+++ scripts/register_uri_parser.py
@@ -2,20 +2,20 @@
 import os
 
 # Path to script
-executable_path = os.path.expanduser("~/pirate_uri/uri_testhandler.py")
+executable_path = os.path.expanduser("~/pirate_uri/uri_app.py")
 
 desktop_entry = f"""
 [Desktop Entry]
-Name=Pirate URI test handler
+Name=Pirate URI App
 Exec=gnome-terminal -- python3 {executable_path} %u; echo "Press Enter to exit"; read
 Type=Application
 MimeType=x-scheme-handler/pirate;
-Comment=Test Handler for Pirate URI's
+Comment=Handler App for Pirate URI's
 Terminal=false
 """
 
 def register_uri_scheme():
-    desktop_path = os.path.expanduser("~/.local/share/applications/pirate-litewallet.desktop")
+    desktop_path = os.path.expanduser("~/.local/share/applications/pirate-uri_app.desktop")
     with open(desktop_path, 'w') as desktop_file:
         desktop_file.write(desktop_entry)
     print("Desktop entry created. Updating database...")
